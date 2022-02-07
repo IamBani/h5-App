16244774e69129e6fa4a6e3000a0058184891463
IamBani <13972186114.com>
2022-02-07 18:04:24 +0800

    1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,13 +4,7 @@
   <button @click="change">点击</button>
   <br/>
   <div>{{mainStore.action}}</div>
-  <van-search
-    v-model="value"
-    show-action
-    placeholder="请输入搜索关键词"
-    @search="onSearch"
-    @cancel="onCancel"
-  />
+
 </template>
 
 <script lang="ts" setup>
@@ -18,13 +12,7 @@ import { ref } from 'vue';
 import useMainStore from '../store/index';
 
 const mainStore = useMainStore();
-const value = ref('');
-const onSearch = (v:string) => {
-  console.log(v);
-};
-const onCancel = (v:string) => {
-  console.log(v);
-};
+
 const change = () => {
   // mainStore.$patch({
   //   num: 10,
